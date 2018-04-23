fa89770e5f426d13dd059a9be6be0c9b7d083612
otonashiryuugen <dylanjvanderwal@gmail.com>
2018-04-24 09:18:20 +1000

    Added date time

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ var MongoURL = "mongodb://127.0.0.1:27017/";
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	var q = url.parse(req.url, true).query;
+	q.dateTime = new Date();
 	var txt = JSON.stringify(q)
 
-	MongoClient.connect(MongoURL, q, function(err, db) {
+	MongoClient.connect(MongoURL, q, txt, function(err, db) {
 		if (err) throw err;
 		var dbo = db.db("RBSN");
 		dbo.collection("data").insertOne(q, function(err, res) {
 			if (err) throw err;
 			console.log("1 document inserted");
+			console.log(txt)
 			db.close();
 		});
 	});
