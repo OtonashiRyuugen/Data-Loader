c458a891566971d684e07d177c5d74191807cd76
otonashiryuugen <dylanjvanderwal@gmail.com>
2018-04-24 08:12:26 +1000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
+var url = require('url');
+var MongoURL = "mongodb://127.0.0.1:27017/";
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var q = url.parse(req.url, true).query;
+	var txt = JSON.stringify(q)
+
+	MongoClient.connect(MongoURL, q, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("RBSN");
+		dbo.collection("data").insertOne(q, function(err, res) {
+			if (err) throw err;
+			console.log("1 document inserted");
+			db.close();
+		});
+	});
+
+	res.end(txt);
+}).listen(8080);
